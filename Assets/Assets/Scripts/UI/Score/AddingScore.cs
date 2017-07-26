325df1e3b8b9c705ab4350e3796ce815c3ad0f51
Christopher Cooke <cookec@gmatc.matc.edu>
2017-07-25 20:53:55 -0500

    5.5 Conversion

--- Assets/Assets/Scripts/UI/Score/AddingScore.cs
+++ Assets/Assets/Scripts/UI/Score/AddingScore.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections;
+﻿/* 11 errors -- Missing Reference -- CC
+ * 
+ * 
+ * 
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -78,3 +82,4 @@ public class AddingScore : MonoBehaviour {
         }
     }
 }
+*/
