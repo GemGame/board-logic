a04935fd52e192c1e939dca23817a2fd0ce68575
Koesterc <Koesterc@gmatc.matc.edu>
2017-07-29 17:41:57 -0500

    fixed minor bug

--- Assets/Assets/Scripts/Managers/Score/ScoreBar.cs
+++ Assets/Assets/Scripts/Managers/Score/ScoreBar.cs
@@ -7,6 +7,8 @@ public class ScoreBar : MonoBehaviour {
     public static Image bar;
     [SerializeField]
     Text _percentage;
+    [SerializeField]
+    Image _bar;
     public static Text percentage;
 
     static ManageScore manageScoreScript;
@@ -14,14 +16,21 @@ public class ScoreBar : MonoBehaviour {
 
     private void Start()
     {
-        bar = gameObject.GetComponent<Image>();
+        bar = _bar;
         percentage = _percentage;
         manageScoreScript = GameObject.Find("Canvas").GetComponent<ManageScore>();
     }
 
     public static void UpdateBar()
     {
-      bar.fillAmount = (float)manageScoreScript.score / manageScoreScript.goal3;
-      percentage.text = Mathf.Floor(bar.fillAmount * 100).ToString()+"%";
+        try
+        {
+            bar.fillAmount = (float)manageScoreScript.score / manageScoreScript.goal3;
+            percentage.text = Mathf.Floor(bar.fillAmount * 100).ToString() + "%";
+        }
+        catch
+        {
+
+        }
     }
 }
